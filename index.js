3b104da597285230e778395c863d6251874b7caa
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-02-12 21:30:18 -0500

    PostgreSQL & Post User

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+const pg = require('pg');
+const path = require('path');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo'
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
@@ -15,22 +18,34 @@ app.get('/', function (req, res) {
 
 app.post('/user', function (req, res) {
 
-    /*
-    • Nombres y apellidos
-• Foto
-• País de origen
-• Ciudad
-• Correo electrónico
-• Y un texto donde explica su interés por la reserve.
-    */
     var names = req.body.names;
     var lastnames = req.body.lastnames;
     var country = req.body.country;
     var city = req.body.city;
     var email = req.body.email;
-    var interest = req.body.interest;
+    var interests = req.body.interests;
 
-    res.send(names + ", " + lastnames);
+    const results = [];
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, (err, client, done) => {
+        // Handle connection errors
+        if (err) {
+            done();
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                data: err
+            });
+        }
+        // SQL Query > Insert Data
+        client.query('INSERT INTO user(names, lastnames, country, city, email, interests) values($1, $2, $3, $4, $5, $6)', [names, lastnames, country, city, email, interests]);
+        // SQL Query > Select Data
+
+        res.send('Usuario Agregado!')
+
+    });
+
+
 });
 
 
