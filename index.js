7357bcf0f1e691971e9d6b799afe27825cdb8f4c
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-02-12 21:58:05 -0500

    Test Query User

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ app.post('/user', function (req, res) {
         // SQL Query > Insert Data
         client.query('INSERT INTO user (names, lastnames, country, city, email, interests) VALUES ($1, $2, $3, $4, $5, $6)', [names, lastnames, country, city, email, interests]);
         // SQL Query > Select Data
-        const query = client.query('SELECT * FROM user ORDER BY id ASC');
-        // Stream results back one row at a time
-        query.on('row', (row) => {
-            results.push(row);
-        });
-        // After all data is returned, close connection and return results
+//        const query = client.query('SELECT * FROM user ORDER BY id ASC');
+//        // Stream results back one row at a time
+//        query.on('row', (row) => {
+//            results.push(row);
+//        });
+//// After all data is returned, close connection and return results
         query.on('end', () => {
             done();
             res.send(results);
