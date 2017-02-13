3d8b5af6dc2f6267a9370659be34c62b67b646be
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-02-12 21:06:57 -0500

    BodyParser & Post User

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,7 +14,23 @@ app.get('/', function (req, res) {
 });
 
 app.post('/user', function (req, res) {
-    res.send('Post esta funcionando')
+
+    /*
+    • Nombres y apellidos
+• Foto
+• País de origen
+• Ciudad
+• Correo electrónico
+• Y un texto donde explica su interés por la reserve.
+    */
+    var names = req.body.names;
+    var lastnames = req.body.lastnames;
+    var country = req.body.country;
+    var city = req.body.city;
+    var email = req.body.email;
+    var interest = req.body.interest;
+
+    res.send(names + ", " + lastnames);
 });
 
 
