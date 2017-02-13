95a6de3f24fc2352e21595a828edf88a9b32772f
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-02-12 21:01:32 -0500

    Test Post

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
-})
+});
+
+app.post('/user', function (req, res) {
+    res.send('Post esta funcionando')
+});
+
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
